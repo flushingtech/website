37f7356b489644895eaf599af5e317c6f6bc993d
dentonzh <denzix@gmail.com>
2023-10-07 14:56:21 -0400

    Add header

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import '../app.css';
+    import Header from './Header.svelte';
+</script>
+
+<Header></Header>
+<slot class="container" />
+
+<style>
+    .container {
+        display: flex;
+    }
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+    import Logo from './Logo.svelte';
+    import Search from './Search.svelte';
+</script>
+
+<div class="navigation">
+    <Logo></Logo>
+    <div class="brand-text">Flushing Tech</div>
+    <Search></Search>
+</div>
+
+<style>
+    .navigation {
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        display: flex;
+        flex-direction: row;
+        background-color: #eaeaea;
+        padding: 0;
+    }
+
+
+    .brand-text {
+        font-weight: bold;
+        font-size: 2rem;
+    }
+</style>
