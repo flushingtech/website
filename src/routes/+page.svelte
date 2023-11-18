35f28996d8224b6d96f8626b7729e5f8dd326d01
AllenChienXXX <allen71090@gmail.com>
2023-11-18 17:52:06 -0500

    Skeleton for "Events"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
+<script>
+    import Event from './Event.svelte'
+</script>
+
 <h1>Flushing Tech Group</h1>
 <p>A bunch of cool people doing cool tech stuff on a cool framework called Svelte.</p>
+<Event></Event>
 
 <style>
     h1 {
