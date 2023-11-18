a977aa63e4727fde1d85a68aef05044b3e3863c0
bikiris <58317286+bikiris@users.noreply.github.com>
2023-11-18 18:07:15 -0500

    Card (#7)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Flushing Tech Group</h1>
+<p>A bunch of cool people doing cool tech stuff on a cool framework called Svelte.</p>
+
+<style>
+    h1 {
+        width: 100%;
+    }
+    p {
+        width: 100%;
+    }
+</style>
---
+++ src/routes/Search.svelte
@@ -0,0 +1,14 @@
+<input class="search-input" placeholder="Search" type="text">
+
+<style>
+    .search-input {
+        border-radius: 0.5rem;
+        font-size: 1.2rem;
+        height: 1.6rem;
+        outline-color: rgb(228, 239, 243);
+        border-color: transparent;
+        margin: 0 0.8rem;
+        padding: 0.2rem 0.5rem;
+        align-items: center;
+    }
+</style>
