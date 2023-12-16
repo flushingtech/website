85df6f39951f8ca981afcd9937a2b2770d96886c
Alex Chan <alex@Alexs-MacBook-Pro.local>
2023-12-16 17:33:41 -0500

    make changes in portfolio code

--- src/routes/Profolio.svelte
+++ src/routes/Profolio.svelte
@@ -1,14 +1,39 @@
+<script>
+let projects = [{
+    "projectName":"Project Name",
+    "purpose":"Purpose of the project",
+    "technologyUsed":"Technology used to complete project",
+    "status":"status of project",
+    "url":"url",
+    "image":"https://placehold.co/600x400/png",
+    "duration":"duration of project",
+    "license":"license used"
+},{
+    "projectName":"Project Name 2",
+    "purpose":"Purpose of the project 2",
+    "technologyUsed":"Technology used to complete project 2",
+    "status":"status of project 2",
+    "url":"url 22222",
+    "image":"https://placehold.co/600x400/png",
+    "duration":"duration of project res lorem ipsum",
+    "license":"license used"
+}]
+
+</script>
+{#each projects as project}
 <div class="card-container">
-    <div class="card" style="width: 18rem;">  
+    <div class="card" style="width: 18rem;">
+          
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <h5 class="card-title">{project.projectName}</h5>
+          <p class="card-text">{project.purpose}</p>
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
-        <img src="https://placehold.co/600x400/png" class="card-img-top" alt="...">
+        <img src="{project.image}" class="card-img-top" alt="...">
+
       </div>
 </div>
-
+{/each}
 <style>
     .card-container{
         display: flex;
