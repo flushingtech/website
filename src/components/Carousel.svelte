c35ca87090c7c6615ecc98b136157228261cb412
Simran Kaur <simran00kk@gmail.com>
2024-04-08 18:05:07 -0400

    added description section to carousel. changed organization of carousel components. Carousel now consists of nested flexboxes.

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -17,21 +17,42 @@
   Our Work
 </div>
 
-<div class="carousel-container relative flex justify-center my-4">
-  <div class="carousel relative max-w-screen-md">
-    <div class="flex items-center justify-center"> 
-      <button on:click={prev} class="carousel-control mr-2 p-2 text-white hover:text-green-500 rounded-full">
-        <span class="material-symbols-outlined text-7xl">chevron_left</span>
-      </button>
-      <img src={projectsData[currentIndex].image_URL} alt={projectsData[currentIndex].title} class="carousel-image">
-      <button on:click={next} class="carousel-control ml-2 p-2 text-white hover:text-green-500 rounded-full">
-        <span class="material-symbols-outlined text-7xl">chevron_right</span>
-      </button>
-    </div>
-    <div class="project-title text-center mt-2 text-4xl"> 
-      {projectsData[currentIndex].title}
-    </div>
+<div class="carousel-container flex items-center justify-center">
+  <div class = "left ml-20 mr-4 ">
+    <button on:click={prev} class="carousel-control text-white hover:text-green-500 rounded-full">
+      <span class="material-symbols-outlined text-8xl">chevron_left</span>
+    </button>
   </div>
+
+  <div class="middle flex-col max-w-6xl">
+      <a>
+        <img class="carousel-image " src={projectsData[currentIndex].image_URL} alt={projectsData[currentIndex].title} >
+      </a>
+      
+      <div class = "caption bg-black bg-opacity-50 mb-4">
+        <div class="project-title text-left mt-2 text-2xl"> 
+          {projectsData[currentIndex].title}
+        </div>
+        <div class="description text-lg">
+          {projectsData[currentIndex].description}
+        </div>
+        <div class="team text-sm">
+          Team: {projectsData[currentIndex].team.join('; ')}
+        
+    
+        </div>
+
+      </div>
+
+  </div>
+
+  <div class = "right mr-20 ml-4 ">
+    <button on:click={next} class="carousel-control text-white hover:text-green-500 rounded-full">
+      <span class="material-symbols-outlined text-8xl">chevron_right</span>
+    </button>
+  </div>
+
+
 </div>
 
 <!-- when icon is clicked (active state) it will get slightly darker -->
