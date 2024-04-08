4d754a22db9c5df72d6e9e5172774a01f155f278
Simran Kaur <simran00kk@gmail.com>
2024-04-08 15:19:47 -0400

    about section in main is more responsive due to added padding and minimum height in css styling

--- src/routes/Main.svelte
+++ src/routes/Main.svelte
@@ -3,58 +3,52 @@
     import LandingImage from './LandingImage.svelte';
 </script>
 
-<div class = 'container'>
+<div class="container">
     <Logo></Logo>
     <LandingImage></LandingImage>
-
 </div>
 
 <div class="about-us-divider">
-    <div class= "about-us-heading">
+    <div class="about-us-heading">
         What does this group do?
     </div>
-    <div class= "about-us-text">
-        We hold freeform, self-organized hackathons where creativity knows no bounds. Whether you're a seasoned pro, aspiring entrepeneur, or passionate hobbyist, our events are tailor-made for tech enthusiasts like you. Bring a laptop, your brightest ideas, and ongoing projects - lets build something together! 
+    <div class="about-us-text">
+        We hold freeform, self-organized hackathons where creativity knows no bounds. Whether you're a seasoned pro, aspiring entrepreneur, or passionate hobbyist, our events are tailor-made for tech enthusiasts like you. Bring a laptop, your brightest ideas, and ongoing projects - let's build something together! 
     </div>
 </div>
 
-
 <style>
-
-    .container{
+    .container {
         display: flex;
         justify-content: space-around;
         margin-left: 1.5rem;
         margin-right: 1.5rem;
         margin-top: 1rem;
         margin-bottom: 2rem;
-
     }
 
-    .about-us-divider{
-        background-image: url('./images/about.png') ;
-        background-size: cover;
-        height: 10em;
+    .about-us-divider {
+        background-image: url('./images/about.png');
+        min-height: 10em; /* Changed height to min-height */
         background-position: center;
-    
+        padding: 1em; /* Added padding for spacing */
+        object-fit: contain;
+        
     }
 
-    .about-us-heading{
+    .about-us-heading {
         font-family: "Bungee";
         margin-right: 1.5em;
         text-align: end;
         font-size: 2.5em;
-
+        color: white; /* Added text color */
     }
-    .about-us-text{
-        background-color: #04010535;
-        margin-left: 2em;
-        margin-right: 2em;
+
+    .about-us-text {
+        background-color: rgba(4, 1, 5, 0.2); /* Changed opacity */
+        margin: 1em; /* Added margin for spacing */
         font-size: 1.2em;  
-        display: flex;
-        justify-content: center;   
+        text-align: center; /* Centered text */
+        color: white; /* Added text color */
     }
-
-
-
 </style>
