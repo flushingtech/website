4a29012fe12ffeab3c0ea550c9853b29cfa47a25
Simran Kaur <simran00kk@gmail.com>
2024-04-08 21:10:18 -0400

    used a grid to style the carousel

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -17,47 +17,79 @@
   Our Work
 </div>
 
-<div class="carousel-container flex items-center justify-center">
-  <div class = "left ml-20 mr-4 ">
-    <button on:click={prev} class="carousel-control text-white hover:text-green-500 rounded-full">
-      <span class="material-symbols-outlined text-8xl">chevron_left</span>
-    </button>
-  </div>
 
-  <div class="middle flex-col max-w-6xl">
+  <div class="wrapper">
+
+    <div class="left">
+      <button on:click={prev} class="carousel-control text-white hover:text-green-500 rounded-full">
+        <span class="material-symbols-outlined text-8xl">chevron_left</span>
+      </button>
+    </div>
+
+    <div class="box2">
       <a>
         <img class="carousel-image " src={projectsData[currentIndex].image_URL} alt={projectsData[currentIndex].title} >
       </a>
-      
-      <div class = "caption bg-black bg-opacity-50 mb-4">
-        <div class="project-title text-left mt-2 text-2xl"> 
-          {projectsData[currentIndex].title}
-        </div>
-        <div class="description text-lg">
-          {projectsData[currentIndex].description}
-        </div>
-        <div class="team text-sm">
-          Team: {projectsData[currentIndex].team.join('; ')}
-        
-    
-        </div>
+    </div>
 
+    <div class="right">
+      <button on:click={next} class="carousel-control text-white hover:text-green-500 rounded-full">
+        <span class="material-symbols-outlined text-8xl">chevron_right</span>
+      </button> 
+    </div>
+
+    <div class="box4">
+      <div class="title">
+        {projectsData[currentIndex].title}
       </div>
-
-  </div>
-
-  <div class = "right mr-20 ml-4 ">
-    <button on:click={next} class="carousel-control text-white hover:text-green-500 rounded-full">
-      <span class="material-symbols-outlined text-8xl">chevron_right</span>
-    </button>
+      <div class="description">
+        {projectsData[currentIndex].description}
+      </div>
+    </div>
   </div>
 
 
-</div>
-
+  
 <!-- when icon is clicked (active state) it will get slightly darker -->
 <style>
-  .carousel-control span.material-symbols-outlined:active {
+  /* .carousel-control span.material-symbols-outlined:active {
     filter: brightness(70%);
-  }
+  } */
+
+  .wrapper {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: auto;
+}
+
+.left {
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row-start: 2;
+  grid-row-end: 3;
+  justify-self: center;
+}
+
+.box2 {
+  grid-column-start: 2;
+  grid-column-end: 5;
+  grid-row-start: 1;
+  grid-row-end: 4;
+}
+
+.right{
+  grid-column-start: 5;
+  grid-column-end: 6;
+  grid-row-start: 2;
+  grid-row-end: 3;
+  justify-self: center;
+
+}
+.box4{
+  grid-column-start: 2;
+  grid-column-end: 5;
+  grid-row-start: 4;
+  grid-row-end: 5;
+}
+
 </style>
