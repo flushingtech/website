be95c221754d19cbb2a290624ed43fc82671caa8
Simran Kaur <simran00kk@gmail.com>
2024-04-11 16:42:45 -0400

    Filled out nav

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -3,11 +3,17 @@
 
 <div class="nav m-4 flex justify-between items-center">
     <div class="top-left-nav text-3xl font-thin" style="font-family: 'Bungee Shade', sans-serif; color: #baf553ff;">Flushing Tech</div>
-    <!-- <div class="redirect flex justify-between w-1/4 text-lg mr-4">
+    <div class="redirect flex justify-around w-1/4 text-lg mr-4">
+        <a href="https://discord.gg/xGgFcZknDR" target="_blank">
+            Discord  
+        </a>
+        <a href="https://github.com/flushingtech/" target="_blank">
+            GitHub
+        </a>
 
-        <div>About Us</div>
-        <div>Our Work</div>
-        <div>Join Us</div>
-    </div> -->
+        <a href="https://www.meetup.com/flushing-tech-meetup-group/" target="_blank">
+            Meetup
+        </a>
+    </div>
 </div>
   
