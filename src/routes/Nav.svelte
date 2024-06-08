137deb3f575d1185ec731b4ec417295ea176c692
Mohammad Haider <arzhdr@gmail.com>
2024-06-08 19:50:09 -0400

    updated nav with hover effects

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -4,16 +4,17 @@
 <div class="nav m-4 flex justify-between items-center">
     <div class="top-left-nav text-3xl font-thin" style="font-family: 'Bungee Shade', sans-serif; color: #baf553ff;">Flushing Tech</div>
     <div class="redirect flex justify-around w-1/4 text-lg mr-4">
-        <a href="https://discord.gg/xGgFcZknDR" >
+        <a class="hover:text-green-500 rounded-full"href="https://discord.gg/xGgFcZknDR" >
             Discord  
         </a>
-        <a href="https://github.com/flushingtech/" >
+        <a  class="hover:text-green-500 rounded-full"href="https://github.com/flushingtech/" >
             GitHub
         </a>
 
-        <a href="https://www.meetup.com/flushing-tech-meetup-group/" >
+        <a  class="hover:text-green-500 rounded-full" href="https://www.meetup.com/flushing-tech-meetup-group/" >
             Meetup
         </a>
     </div>
 </div>
   
+
