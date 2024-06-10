fd4fd69af5d455f2681a123439a4f714275fe476
Simran Kaur <simran00kk@gmail.com>
2024-06-10 12:53:17 -0400

    added semantic html to nav bar and focus styling to links

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -1,20 +1,23 @@
 <script>
 </script>
 
-<div class="nav m-4 flex justify-between items-center">
-    <div class="top-left-nav text-3xl font-thin" style="font-family: 'Bungee Shade', sans-serif; color: #baf553ff;">Flushing Tech</div>
-    <div class="redirect flex justify-around w-1/4 text-lg mr-4">
-        <a class="hover:text-green-500 rounded-full"href="https://discord.gg/xGgFcZknDR" >
-            Discord  
-        </a>
-        <a  class="hover:text-green-500 rounded-full"href="https://github.com/flushingtech/" >
-            GitHub
-        </a>
-
-        <a  class="hover:text-green-500 rounded-full" href="https://www.meetup.com/flushing-tech-meetup-group/" >
-            Meetup
-        </a>
-    </div>
-</div>
+<nav class="nav m-4" >
+    <ul class="flex justify-between items-center list-none">
+        <li>
+            <a href="https://flushingtech.org" class="top-left-nav text-3xl font-thin" style="font-family: 'Bungee Shade', sans-serif; color: #baf553ff;">Flushing Tech</a>
+        </li>
+        <li class="redirect flex justify-around w-1/4 text-lg mr-4">
+            <a class="hover:text-green-500 rounded-full focus:outline-none focus:ring-2 focus:ring-green-500" href="https://discord.gg/xGgFcZknDR">
+                Discord
+            </a>
+            <a class="hover:text-green-500 rounded-full focus:outline-none focus:ring-2 focus:ring-green-500" href="https://github.com/flushingtech/">
+                GitHub
+            </a>
+            <a class="hover:text-green-500 rounded-full focus:outline-none focus:ring-2 focus:ring-green-500" href="https://www.meetup.com/flushing-tech-meetup-group/">
+                Meetup
+            </a>
+        </li>
+    </ul>
+</nav>
   
 
