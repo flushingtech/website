e9db7f5f7da5b1b118ebfebc710dfaae38939815
Mohammad Ariz Haider <38084115+Ares-93@users.noreply.github.com>
2024-06-10 11:35:23 -0400

    Ares/using-semantic-html (#33)
    * updated nav with hover effects
    
    * added semantic html

--- src/routes/Main.svelte
+++ src/routes/Main.svelte
@@ -8,14 +8,14 @@
     <LandingImage></LandingImage>
 </div>
 
-<div class="about-us-divider">
-    <div class="about-us-heading text-4xl text-right mr-6 text-white">
+<section class="about-us-divider">
+    <h1 class="about-us-heading text-4xl text-right mr-6 text-white">
         What does this group do?
-    </div>
-    <div class="about-us-text bg-opacity-20 p-4 text-xl text-center text-white">
+    </h1>
+    <p class="about-us-text bg-opacity-20 p-4 text-xl text-center text-white">
         We hold freeform, self-organized hackathons where creativity knows no bounds. Whether you're a seasoned pro, aspiring entrepreneur, or passionate hobbyist, our events are tailor-made for tech enthusiasts like you. Bring a laptop, your brightest ideas, and ongoing projects - let's build something together! 
-    </div>
-</div>
+    </p>
+</section>
 
 
 <style>
